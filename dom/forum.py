b9f6bf5aaaa2cce8f05c3d29d6f3d0a238374363
pietervanoverbeke <pieter.vanoverbeke@student.kdg.be>
2017-12-22 11:42:26 +0100

    smth bitcoin, ripple, btc

--- dom/forum.py
+++ dom/forum.py
@@ -1,6 +1,13 @@
+from model.collection_model import CollectionModel
+
+
 class Forum:
     def __init__(self):
         self.threads = []
+        self.collection_model = 0
 
     def add_thread(self, thread):
         self.threads.append(thread)
+
+    def make_models(self):
+        self.collection_model = CollectionModel(self)
--- test/testInput.py
+++ test/testInput.py
@@ -8,4 +8,6 @@ class TestReader(unittest.TestCase):
     def test_reader(self):
         fileuris = ["kakhoofd", "u mama"]
         forum = Xml_convertor.convert_data("dfss")
+        forum.make_models()
+        print(forum.collection_model.corpus)
         assert_that(len(forum.threads), equal_to(379))
