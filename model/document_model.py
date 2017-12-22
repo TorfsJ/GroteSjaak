336ad941970ae20004e99ba7c64ec5f9224304de
jellisT <jellis.torfs@student.kdg.be>
2017-12-22 15:11:53 +0100

    document_model:

--- model/document_model.py
+++ model/document_model.py
@@ -7,6 +7,7 @@ class DocumentModel:
     def __init__(self, text):
         self.document_terms = PreProcessor.processText(text)
         self.tfidx = FrequencyHelper.calculate_frequencies(text)
+        self.rank_value = -1
 
     def prob_document_given_query(self, query_model):
         return reduce(lambda y, z: y * z, map(lambda x: self.tfidx.get(x), query_model))
